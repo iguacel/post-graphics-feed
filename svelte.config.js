062d3c26c456eb79932c09d760098e88bea6cc3e
Jose A. Álvarez <infoiguacel@gmail.com>
2024-06-14 11:39:53 +0200

    new node

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,16 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import sveltePreprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
+    preprocess: sveltePreprocess(),
 
-	kit: {
-		appDir: 'app',
-		adapter: adapter(),
+    kit: {
+        adapter: adapter(),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/post-graphics-feed' : ''
-		}
-	}
+            base: process.env.NODE_ENV === 'production' ? '/post-graphics-feed' : '',
+        }
+    }
 };
 
 export default config;
