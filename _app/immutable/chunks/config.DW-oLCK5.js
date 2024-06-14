477efed7dc4212090b14d4d6e46a5aaadd0b1f07
Jose A. Álvarez <infoiguacel@gmail.com>
2024-06-14 11:43:53 +0200

    Updates

---
+++ _app/immutable/chunks/config.DW-oLCK5.js
@@ -0,0 +1,1 @@
+const s="Washington Post Graphics feed",t="A curated list of recent articles published by The Washington Post's graphics team.",e="https://luissevillano.net/post-graphics-feed/index.html",i="https://luissevillano.net/post-graphics-feed/cover.jpg";export{t as a,e as b,i as c,s};
---
+++ _app/immutable/entry/start.CV1Q5UUK.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.Cvyty2Ij.js";export{t as start};
